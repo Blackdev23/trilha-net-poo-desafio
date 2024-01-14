8ffdf1159bf1ce3f10db5ee87c7cb4d7081ca0a7
Erasmo Junior <enojr2604dev@gmail.com>
2024-01-13 22:19:44 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+namespace POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Smartphone NOKIA");
+            Smartphone nokia = new Nokia("123456", "Modelo 1", "111111", 64);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("Whatsapp");
+
+            Console.WriteLine("\nSmartphone IPHONE");
+            Smartphone iphone = new Iphone("654321", "Modelo 2", "111111", 128);
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Telegram");
+        }
+    }
+}
